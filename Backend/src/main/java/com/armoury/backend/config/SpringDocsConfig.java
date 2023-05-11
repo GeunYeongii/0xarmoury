09f0110cb23e034b999777de8a2dec3152a3ee0a
gayoung <iona15813@gmail.com>
2023-05-11 12:13:14 +0900

    Swagger 설정2

--- Backend/src/main/java/com/armoury/backend/config/SpringDocsConfig.java
+++ Backend/src/main/java/com/armoury/backend/config/SpringDocsConfig.java
@@ -1,19 +1,19 @@
 package com.armoury.backend.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocsConfig {
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
-                .title("Capstone Cyber1 Project - API Document")
-                .version("v0.0.1")
-                .description("사이버 무기고 프로젝트의 API 명세서입니다.");
+                .title("캡스톤 프로젝트 API Document")
+                .version("1.0.0")
+                .description("Cyber1 - 사이버 무기고 프로젝트");
         return new OpenAPI()
                 .components(new Components())
                 .info(info);
--- Backend/src/main/java/com/armoury/backend/tools/ToolController.java
+++ Backend/src/main/java/com/armoury/backend/tools/ToolController.java
@@ -3,6 +3,10 @@ package com.armoury.backend.tools;
 import com.armoury.backend.config.BaseException;
 import com.armoury.backend.config.BaseResponse;
 import com.armoury.backend.tools.model.GetToolRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tools")
+@Tag(name = "Tools", description = "해킹 도구와 관련된 기능 & 정보 제공")
 public class ToolController {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -21,6 +26,7 @@ public class ToolController {
     }
 
     @ResponseBody
+    @Operation(summary = "공격 도구 정보 조회", description = "toolIdx 사용하여 공격 도구 정보 조회")
     @GetMapping("/{toolIdx}")
     public BaseResponse<GetToolRes> getToolByIdx (@PathVariable("toolIdx")int toolIdx){
         try{
