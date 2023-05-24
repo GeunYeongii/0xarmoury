332bc0fd492dd22201ff2e41037e0800410d4ba5
gayoung <iona15813@gmail.com>
2023-05-25 03:08:21 +0900

    Chore: gitignore

--- Backend/src/main/java/com/armoury/backend/config/Constant.java
+++
@@ -1,5 +0,0 @@
-package com.armoury.backend.config;
-
-public class Constant {
-}
-
--- Backend/src/main/java/com/armoury/backend/config/secret/Secret.java
+++
@@ -1,5 +0,0 @@
-package com.armoury.backend.config.secret;
-
-public class Secret {
-    public static String JWT_SECRET_KEY = "UwKYibQQgkW7g-*k.ap9kjeASdk9F#DXF4vnt4P3sJWtNu";
-}
