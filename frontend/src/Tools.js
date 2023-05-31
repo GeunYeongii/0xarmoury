64c5ad02fd1cd63196eff52d1d3da796ba907117
stella <stella2837@naver.com>
2023-05-31 12:16:33 +0900

    text 수정

--- frontend/src/Tools.js
+++ frontend/src/Tools.js
@@ -135,12 +135,12 @@ function Tools(){
     const [fullscreen, setFullscreen] = useState(false);
 
     const [selectedId, setSelectedId] = useState(null);
-    const [selectedLabel, setSelectedLabel] = useState("Title");
-    const [Toolcode, setToolCode] = useState("여기는 표식체계 자리");
-    const [Tooldefintion, setTooldefinition]=useState("여기는 툴 설명\n아 여기네ㅎ");
-    const [Tooloption, setTooloption]=useState("여기는 옵션 설명\n제발돼라ㅏㅏㅏㅏ");
+    const [selectedLabel, setSelectedLabel] = useState("[select tool]");
+    const [Toolcode, setToolCode] = useState("");
+    const [Tooldefintion, setTooldefinition]=useState("");
+    const [Tooloption, setTooloption]=useState("");
     const [ToolMITRE, setToolMITRE]=useState([]);
-    const [ToolWiki, setToolWiki] = useState("기본 위키 내용\n여기두");
+    const [ToolWiki, setToolWiki] = useState("");
     const [isEditing, setIsEditing] = useState(false);
     const [editedWiki, setEditedWiki] = useState(ToolWiki);
     const [toollist, setToollist] = useState([]);
